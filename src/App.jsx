853bc4efc2ff686af85dddc901641606826dceb9
Marcos Afonso Vieira <marcos.vieira@pelicano.br>
2023-07-27 10:14:38 -0300

    tarefa e lista no mesmo component

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,14 @@ import Titulo from './Titulo.jsx'
 import Task from './Task.jsx'
 import zelda from './assets/zelda2.jpg';
 import luck from './assets/luck.jpg';
-import ListaTask from './listaTask.jsx';
+import ListaTask from './ListaTask.jsx';
 
 function App() {
   // const [count, setCount] = useState(0)
   return (
     <div className='cardPrincipal'>
-      <h2>Todo App</h2>
+      <h2>Todo App2</h2>
     <Titulo />
-    <ListaTask />
     </div>
   )
 }
--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -7,57 +7,74 @@ import api from "./services/api";
 
 
 function Titulo({ descricao }) {
+  // Dados dos campos do formulário
   const [dados, setDados] = useState({
     title: ''
   });
+  const valueInput = (e) => setDados({ ...dados, [e.target.name]: e.target.value });
 
-// Receber os dados dos campos do formulário
-const valueInput = (e) => setDados({ ...dados, [e.target.name]: e.target.value });
-
-// Executar a função quando o usuário clicar no botão do formulário
-const addTask = async (e) => {
-  // Bloquear o recarregamento da página
-  e.preventDefault();
-
-  // Criar a constante com os dados do cabeçalho
-  const headers = {
-    'headers': {
-    // Indicar que será enviado os dados em formato de objeto
-    'Content-Type': 'application/json'
-    }
-  };
-
-  // Fazer a requisição para o servidor utilizando axios, indicando o método da requisição, o endereço, enviar os dados do formulário e o cabeçalho
-  await axios.post('http://localhost:3000/tasks', dados, headers)
-    .then((response) => { // Acessa o then quando a API retornar status 200
-      // Atribuir a mensagem no state message
-      //console.log(response.data.mensagem);
-      // setMessage(response.data.nomeTarefa);
-      // Limpar os dados do state e os dados dos campos do formulário
-      setDados({
-        nomTarefa: ''
+  //lista tasks
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    api.get("/tasks") // Altere a URL para buscar todas as tarefas
+      .then((response) => setTasks(response.data))
+      .catch((err) => {
+        console.error("Ops! Ocorreu um erro: " + err);
       });
-    }).catch((err) => { // Acessa o catch quando a API retornar erro
-      // Atribuir a mensagem no state message
-      //console.log(err.response.data.mensagem);
-      if (err.response) {
-        // setMessage(err.response.data.nomeTarefa);
-      } else {
-        // setMessage("Erro: Tente novamente mais tarde ou entre contato com ...!");
-      }
-    });
-}
+  }, []);
 
-  
+  // Executar a função quando o usuário clicar no botão do formulário
+  const addTask = async (e) => {
+    // Bloquear o recarregamento da página
+    e.preventDefault();
+
+    // Criar a constante com os dados do cabeçalho
+    const headers = {
+      'headers': {
+        // Indicar que será enviado os dados em formato de objeto
+        'Content-Type': 'application/json'
+      }
+    };
+
+    // Fazer a requisição para o servidor utilizando axios, indicando o método da requisição, o endereço, enviar os dados do formulário e o cabeçalho
+    await axios.post('http://localhost:3000/tasks', dados, headers)
+      .then((response) => { // Acessa o then quando a API retornar status 200
+        // Atribuir a mensagem no state message
+        //console.log(response.data.mensagem);
+        // setMessage(response.data.nomeTarefa);
+        // Limpar os dados do state e os dados dos campos do formulário
+        setTasks([...tasks, response.data]);
+        
+        setDados({
+          nomTarefa: ''
+        });
+      }).catch((err) => { // Acessa o catch quando a API retornar erro
+        // Atribuir a mensagem no state message
+        //console.log(err.response.data.mensagem);
+        if (err.response) {
+          // setMessage(err.response.data.nomeTarefa);
+        } else {
+          // setMessage("Erro: Tente novamente mais tarde ou entre contato com ...!");
+        }
+      });
+  }
+
   // const [count, setCount] = useState(0)
   return (
     <div className="card">
       <form onSubmit={addTask}>
-        <input type="text" className="inputText" name="title" placeholder="Nome Tarefa" onChange={valueInput} />
+        <input type="text" className="inputText" name="title" placeholder="Nome Tarefa" 
+        onChange={valueInput}/>
         <button type="submit" className="btnAdd">Adicionar</button>
       </form>
+
+      <h2>Lista de Tarefas</h2>
+      {tasks.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
     </div>
   )
+
 }
 
 export default Titulo
